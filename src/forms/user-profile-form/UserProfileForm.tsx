86c6e5a2bf43ddb436452775ef22d50b3bde581a
Bijay Khapung <khapungbj84@gmail.com>
2024-07-14 16:38:16 +0545

    Add restaurant detail page, Checkout feature

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -30,9 +30,17 @@ type Props = {
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
   currentUser: User;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ currentUser, isLoading, onSave }: Props) => {
+const UserProfileForm = ({
+  currentUser,
+  isLoading,
+  onSave,
+  title = "User Profile",
+  buttonText = "Submit",
+}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -46,10 +54,10 @@ const UserProfileForm = ({ currentUser, isLoading, onSave }: Props) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSave)}
-        className="space-y-4 bg-gray-200 rounded-lg md:p-10"
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <h2 className="text-2xl font-bold">{title}</h2>
           <FormDescription>
             View and change your profile information here
           </FormDescription>
@@ -127,7 +135,7 @@ const UserProfileForm = ({ currentUser, isLoading, onSave }: Props) => {
           <LoadingButton />
         ) : (
           <Button type="submit" className="bg-orange-500">
-            Submit
+            {buttonText}
           </Button>
         )}
       </form>
---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,148 @@
+import { useParams } from "react-router-dom";
+import { useGetRestaurant } from "../api/RestaurantApi";
+import { AspectRatio } from "../components/ui/aspect-ratio";
+import RestaurantInfo from "../components/RestaurantInfo";
+import MenuItem from "../components/MenuItem";
+import { useState } from "react";
+import OrderSummary from "../components/OrderSummary";
+import { MenuItem as MenuItemType } from "../types";
+import { Card, CardFooter } from "../components/ui/card";
+import CheckoutButton from "../components/CheckoutButton";
+import { UserFormData } from "../forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "../api/OrderApi";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+const DetailPage = () => {
+  const { restaurantId } = useParams();
+
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartitems-${restaurantId}`);
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
+
+  const { createCheckoutSession, isLoading: isCheckoutLoading } =
+    useCreateCheckoutSession();
+
+  const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+
+      let updatedCartItems: CartItem[];
+
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            ...menuItem,
+            quantity: 1,
+          },
+        ];
+      }
+      sessionStorage.setItem(
+        `cartitems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+      return updatedCartItems;
+    });
+  };
+
+  const removeFromCart = (cartItem: CartItem) => {
+    setCartItems((prevCartItems) => {
+      const updatedCartItems = prevCartItems.filter(
+        (item) => item._id !== cartItem._id
+      );
+      sessionStorage.setItem(
+        `cartitems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+      return updatedCartItems;
+    });
+  };
+
+  const onCheckout = async (userFormData: UserFormData) => {
+    if (!restaurant) {
+      return;
+    }
+
+    const checkoutData = {
+      cartItems: cartItems.map((cartItem) => ({
+        menuItemId: cartItem._id,
+        name: cartItem.name,
+        quantity: cartItem.quantity.toString(),
+      })),
+      restaurantId: restaurant._id,
+      deliveryDetails: {
+        name: userFormData.name,
+        addressLine1: userFormData.addressLine1,
+        city: userFormData.city,
+        country: userFormData.country,
+        email: userFormData.email as string,
+      },
+    };
+
+    const data = await createCheckoutSession(checkoutData);
+    window.location.href = data.url;
+  };
+
+  if (!restaurant || isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <div className="flex flex-col gap-10">
+      <AspectRatio ratio={16 / 5}>
+        <img
+          src={restaurant.imageUrl}
+          className="object-cover h-full w-full rounded-md"
+        />
+      </AspectRatio>
+      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+        <div className="flex flex-col gap-4">
+          <RestaurantInfo restaurant={restaurant} />
+          <span className="text-2xl font-bold tracking-tight">Menu</span>
+          {restaurant.menuItems.map((menuItem) => (
+            <MenuItem
+              key={menuItem._id}
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
+          ))}
+        </div>
+        <div>
+          <Card>
+            <OrderSummary
+              restaurant={restaurant}
+              cartItems={cartItems}
+              removeFromCart={removeFromCart}
+            />
+            <CardFooter>
+              <CheckoutButton
+                disabled={cartItems.length === 0}
+                onCheckout={onCheckout}
+                isLoading={isCheckoutLoading}
+              />
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailPage;
